54bd297ddaa634a53f8fb0c9cf47a01e9966fe54
Erickson Siqueira da Silva <83774878+EricksonSiqueira@users.noreply.github.com>
2021-09-26 21:24:04 -0300

    Melhorando o carrinho

--- script.js
+++ script.js
@@ -129,7 +129,7 @@ function cartItemClickListener(event) {
 function createCartItemElement({ id: sku, title: name, price: salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.innerText = `${name} | PREÇO: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
   return li;
 }
